b999cad2da85a0a982508d2eca3121f6f504f3bb
Akeling <2818872279@qq.com>
2023-11-08 16:16:23 +0800

    second

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import warnings
+import requests
+import json
+import re
+import xlwings
+
+from collections import Counter
+
+_headers = { }
 
 
-def print_hi(name):
-
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
+warnings.filterwarnings("ignore")
+# 忽略不安全警告
+def get_html(url):
+    # 获取请求
+    res = requests.get(url).text
+    # 将获取的网页json编码字符串转换为python对象
+    json_dict = json.loads(res)
+    # print(json_dict)
+    return json_dict
 
 if __name__ == '__main__':
-    print_hi('FruitFlash!!!')
-
-
+    url = ;
+    get_html(url);
